a109f73f25bf0147d252a687594b905f88a7d664
tekkoc <tekkoc@gmail.com>
2022-07-27 08:54:11 +0900

    画像形式をppmからpngに

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,45 @@
 mod consts;
 mod rayt;
 
-use std::{fs, fs::File, io::prelude::*, path::Path};
-
-struct Color([f64; 3]);
+use image::{Rgb, RgbImage};
+use rayt::float3::*;
+use std::{collections::HashMap, fs, path::Path};
 
 const IMAGE_WIDTH: u32 = 200;
 const IMAGE_HEIGHT: u32 = 100;
 
-const OUTPUT_FILENAME: &str = "render.ppm";
-const BUCKUP_FILENAME: &str = "render_bak.ppm";
+const OUTPUT_FILENAME: &str = "render.png";
+const BUCKUP_FILENAME: &str = "render_bak.png";
 
 fn main() {
     backup();
 
-    let mut pixels: Vec<Color> = Vec::with_capacity((IMAGE_WIDTH * IMAGE_HEIGHT) as usize);
+    let mut pixels = HashMap::<(u32, u32), Color>::new();
 
     for j in 0..IMAGE_HEIGHT {
-        let par_iter = (0..IMAGE_WIDTH).into_iter().map(|i| {
-            Color([
-                i as f64 / IMAGE_WIDTH as f64,
-                j as f64 / IMAGE_HEIGHT as f64,
-                0.5,
-            ])
+        for i in 0..IMAGE_WIDTH {
+            pixels.insert(
+                (i, j),
+                Color::new(
+                    i as f64 / IMAGE_WIDTH as f64,
+                    j as f64 / IMAGE_HEIGHT as f64,
+                    0.5,
+                ),
+            );
+        }
+    }
+
+    let mut img = RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+    img.enumerate_pixels_mut()
+        .collect::<Vec<(u32, u32, &mut Rgb<u8>)>>()
+        .iter_mut()
+        .for_each(|(x, y, pixel)| {
+            let color = pixels.get(&(*x, *y)).unwrap();
+            pixel[0] = color.r();
+            pixel[1] = color.g();
+            pixel[2] = color.b();
         });
-
-        let mut line_pixels: Vec<_> = par_iter.collect();
-        pixels.append(&mut line_pixels);
-    }
-
-    save_ppm(String::from("render.ppm"), &pixels).unwrap();
-}
-
-fn save_ppm(filename: String, pixels: &[Color]) -> std::io::Result<()> {
-    let mut file = File::create(filename)?;
-    writeln!(file, "P3")?;
-    writeln!(file, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT)?;
-    writeln!(file, "255")?;
-
-    for Color([r, g, b]) in pixels {
-        let to255 = |x| (x * 255.99) as u8;
-        writeln!(file, "{} {} {}", to255(r), to255(g), to255(b))?;
-    }
-    file.flush()?;
-
-    Ok(())
+    img.save(OUTPUT_FILENAME).unwrap();
 }
 
 fn backup() {
