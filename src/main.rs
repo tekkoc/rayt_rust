310ae30fb12f04a454775aa447566385fe3131cd
tekkoc <tekkoc@gmail.com>
2022-08-01 09:41:18 +0900

    add ball

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,32 @@ use rayt::ray::*;
 use rayt::window::*;
 use std::{fs, path::Path};
 
-const IMAGE_WIDTH: u32 = 200;
-const IMAGE_HEIGHT: u32 = 100;
+const IMAGE_WIDTH: u32 = 400;
+const IMAGE_HEIGHT: u32 = 200;
 
 const OUTPUT_FILENAME: &str = "render.png";
 const BUCKUP_FILENAME: &str = "render_bak.png";
 
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * ray.direction.dot(oc);
+    let c = oc.dot(oc) - radius.powi(2);
+    let d = b * b - 4.0 * a * c;
+    if d < 0.0 {
+        -1.0
+    } else {
+        return (-b - d.sqrt()) / (2.0 * a);
+    }
+}
+
 fn color(ray: &Ray) -> Color {
+    let c = Point3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(c, 0.5, &ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - c).normalize();
+        return 0.5 * (n + Vec3::one());
+    }
     let d = ray.direction.normalize();
     let t = 0.5 * (d.y() * 1.0);
     Color::new(0.5, 0.7, 1.0).lerp(Color::one(), t)
@@ -42,7 +61,7 @@ fn main() {
         .par_iter_mut()
         .for_each(|(x, y, pixel)| {
             let u = *x as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = *y as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let v = (IMAGE_HEIGHT - *y - 1) as f64 / (IMAGE_HEIGHT - 1) as f64;
             let ray = camera.ray(u, v);
             let rgb = color(&ray).to_rgb();
 
